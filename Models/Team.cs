79774de67459c22db142f4edc0978bb61af71e14
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-12-25 00:35:03 +0300

    first upload

---
+++ Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelcukElektromobilWebsite.Models
+{
+    public class Team
+    {
+        public string Id { get; set; }
+        public string NameSurname { get; set; }
+        public string Photo { get; set; }
+    }
+}
