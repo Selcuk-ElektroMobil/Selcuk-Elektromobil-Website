1f7eed7c57133446fee54eeba5255d74158f0e42
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-12-27 00:26:15 +0300

    Create About Pages

---
+++ Controllers/AnasayfaController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelcukElektromobilWebsite.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+
+        /* ANASAYFA */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        /* HAKKIMIZDA SAYFALARI */
+        public IActionResult BizKimiz()
+        {
+            return View();
+        }
+
+        public IActionResult Misyonumuz()
+        {
+            return View();
+        }
+
+        public IActionResult Vizyonumuz()
+        {
+            return View();
+        }
+
+        public IActionResult SSS()
+        {
+            return View();
+        }
+
+
+
+        /* EKİBİMİZ SAYFALARI */
+        public IActionResult Akademisyenler()
+        {
+            return View();
+        }
+
+        public IActionResult ElektronikTakimi()
+        {
+            return View();
+        }
+
+        public IActionResult MekanikTakimi()
+        {
+            return View();
+        }
+
+        public IActionResult YazilimTakimi()
+        {
+            return View();
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -43,7 +43,7 @@ namespace SelcukElektromobilWebsite
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Anasayfa}/{action=Index}/{id?}");
             });
         }
     }
