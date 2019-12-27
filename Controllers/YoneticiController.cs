d571352efa38546d5905fb0b88fa02dd374fe6c1
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-12-28 00:58:42 +0300

    Created Layout For Admin

---
+++ Controllers/YoneticiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelcukElektromobilWebsite.Controllers
+{
+    public class YoneticiController : Controller
+    {
+        
+        /* ANASAYFA */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
