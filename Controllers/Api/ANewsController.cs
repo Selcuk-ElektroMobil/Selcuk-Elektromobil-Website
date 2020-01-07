936a326b2c9a309629a6ea94d7d05a58e85f0f6a
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2020-01-07 23:38:56 +0300

    Add News Api To System

---
+++ Controllers/Api/ANewsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Saklambac.NetCore.Database;
+using SelcukElektromobilWebsite.Models;
+
+namespace SelcukElektromobilWebsite.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/ANews")]
+    public class ANewsController : Controller
+    {
+
+        SaklambacDb<News> newsDb = new SaklambacDb<News>();
+
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(newsDb.GetAll());
+        }
+
+        [HttpGet("{newsId}")]
+        public async Task<ActionResult> GetByNewsId(string newsId)
+        {
+            return Ok(newsDb.GetOneById(newsId));
+        }
+
+    }
+}
